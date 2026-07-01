8492e42119d9298be55fa47379bbf5a0a300ffb0
Teemo Tebest <teemo.tebest@gmail.com>
2026-07-01 19:10:47 +0200

    Update focus chart: simplified charts, value labels, connecting areas, green brand color backgrounds

--- src/jsx/components/minisite/Footer.jsx
+++ src/jsx/components/minisite/Footer.jsx
@@ -1,20 +1,24 @@
 import './Footer.css';
 
 const defaultLanguageLinks = [
-  { label: 'Français', url: '#' },
-  { label: 'Español', url: '#' },
-  { label: 'العربية', url: '#' },
-  { label: '简体中文', url: '#' },
-  { label: 'Русский', url: '#' },
-  { label: 'Português', url: '#' }
+  { label: 'English', url: false },
+  { label: 'Français', url: false },
+  { label: 'Español', url: false },
+  { label: 'العربية', url: false },
+  { label: '简体中文', url: false },
+  { label: 'Русский', url: false },
+  { label: 'Português', url: false },
+  { label: 'Kiswahili', url: false },
+  { label: 'Urdu اردو', url: false },
+  { label: 'Hindi हिंदी', url: false }
 ];
 
 const defaultMediaLinks = [
-  { label: 'Photos', url: '#' },
-  { label: 'Digital assets', url: '#' }
+  { label: 'Photos', url: false },
+  { label: 'Digital assets', url: false }
 ];
 
-function Footer({ languageLinks = defaultLanguageLinks, launchEventTitle = 'WIR 2026 launch event', launchEventUrl = 'https://player.vimeo.com/video/000000001', mediaLinks = defaultMediaLinks, reportUrl = '#', videoTitle = 'UNCTAD World Investment Report 2026', videoUrl = 'https://player.vimeo.com/video/000000000' }) {
+function Footer({ languageLinks = defaultLanguageLinks, launchEventTitle = '', launchEventUrl = '', mediaLinks = defaultMediaLinks, reportUrl = '#', title = '' }) {
   return (
     <div className="footer_container">
       <h2>What do you want to do next?</h2>
@@ -26,38 +30,50 @@ function Footer({ languageLinks = defaultLanguageLinks, launchEventTitle = 'WIR 
       <div className="footer_elements">
         <div className="footer_element">
           <div className="footer_content">
-            <h3>Watch the video</h3>
-            <div className="iframe_container iframe_16_9">
-              <iframe allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" frameBorder="0" src={videoUrl} title={videoTitle} />
-            </div>
+            {languageLinks[0].url && (
+              <>
+                <h3>Watch the video</h3>
+                <div className="iframe_container iframe_16_9">
+                  <iframe allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" frameBorder="0" src={languageLinks[0].url} title={title}></iframe>
+                </div>
+              </>
+            )}
             {languageLinks.length > 0 && (
               <ul className="language_links">
                 <li>
                   {languageLinks.map((link, i) => (
                     <span key={link.label}>
-                      {i > 0 && ', '}
-                      <a href={link.url} target="_blank" rel="noreferrer">
-                        {link.label}
-                      </a>
+                      {link.url && i > 0 && ', '}
+                      {link.url && i > 0 && (
+                        <a href={link.url} target="_blank" rel="noreferrer">
+                          {link.label}
+                        </a>
+                      )}
                     </span>
                   ))}
                 </li>
               </ul>
             )}
-            <h4>Watch the launch event</h4>
-            <div className="iframe_container iframe_16_9">
-              <iframe allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" frameBorder="0" src={launchEventUrl} title={launchEventTitle} />
-            </div>
-            <p>{launchEventTitle}</p>
+            {launchEventUrl && (
+              <>
+                <h4>Watch the launch event</h4>
+                <div className="iframe_container iframe_16_9">
+                  <iframe allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" frameBorder="0" src={launchEventUrl} title={launchEventTitle} />
+                </div>
+                <p>{launchEventTitle}</p>
+              </>
+            )}
             {mediaLinks.length > 0 && (
               <div>
-                <h4>Media assets</h4>
+                {(defaultMediaLinks[0].url !== false || defaultMediaLinks[1].url !== false) && <h4>Media assets</h4>}
                 <ul>
                   {mediaLinks.map(link => (
                     <li key={link.label}>
-                      <a href={link.url} target="_blank" rel="noreferrer">
-                        {link.label}
-                      </a>
+                      {link.url !== false && (
+                        <a href={link.url} target="_blank" rel="noreferrer">
+                          {link.label}
+                        </a>
+                      )}
                     </li>
                   ))}
                 </ul>
